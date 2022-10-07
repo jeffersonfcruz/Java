b487767105df2fa6cff2ddb99d704aebb4fc09ed
JEFFERSON WILSON FERREIRA DA CRUZ <jefferson.wfcruz@senacsp.edu.br>
2022-10-07 17:23:00 -0300

    07/10/2022

--- Java/Livraria/src/model/DAO.java
+++ Java/Livraria/src/model/DAO.java
@@ -4,41 +4,40 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 /**
- * Classe Modelo - Conex√£o com o banco
- * @author Jefferson Cruz
+ * Classe modelo - Conex„o com o banco
+ * 
+ * @author Professor JosÈ de Assis
  *
  */
-
 public class DAO {
-	//Par√¢metros de conex√£o
+	// Par‚metros de conex„o
 	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String url = "jdbc:mysql://10.26.49.127:3306/papelariajefferson";
+	private String url = "jdbc:mysql://10.26.49.143:3306/lojinha";
 	private String user = "root";
 	private String password = "123@senac";
 	
 	/**
-	 * M√©todo respons√°vel pela conex√£o
+	 * MÈtodo respons·vel pela conex„o
 	 * @return con / null
 	 */
-			
-
 	public Connection conectar() {
-		
-		//Objeto usado para conex√£o
+		// objeto usado para conex„o
 		Connection con = null;
-		
-		//Tratamento de exce√ß√µes
-		
+		// tratamento de exceÁıes
 		try {
-		//Uso do Driver
+			// uso do driver
 			Class.forName(driver);
+			// estabelecer a conex„o
 			con = DriverManager.getConnection(url, user, password);
 			return con;
-		}
-		
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 			return null;
 		}
 	}
 }
+
+
+
+
+
